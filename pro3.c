a605485cd4174580ae94f478b8965d3a290590ae
abishek007 <abisheksrv@gmail.com>
2017-02-18 21:08:32 +0530

    Add files via upload

---
+++ pro3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	char a;
+	scanf("%c",&a);
+	if(a=='A'||a=='E'||a=='I'||a=='O'||a=='U'||a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
+		printf("vowel\n");
+	}
+	else{
+		printf("not vowel\n");
+	}
+}
