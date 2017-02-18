e3415fc21142cdbf760a91f296b36b6eefd1931f
abishek007 <abisheksrv@gmail.com>
2017-02-18 19:59:01 +0530

    Update pro1.c

--- pro1.c
+++ pro1.c
@@ -3,11 +3,15 @@
 int main(void) {
 	int n;
 	scanf("%d",&n);
+	if(n!=0){
 	if(n>0){
 		printf("positive\n");
 	}
 	else{
 		printf("negative\n");
+	}}
+	else{
+		printf("zero\n");
 	}
 	return 0;
 }
